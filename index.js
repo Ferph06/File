ab1cd9f2b26daa1aae165dd0ad6a4acc151e7767
unknown <fernando.perez@sferea.com>
2017-11-21 15:33:55 -0600

    cambio de rutas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app=express();
 const port=5050;
 const api=require('./routes');
-
+const path='C:/opt/sfereaSystems/biossman/pdf/';
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 //Headers
@@ -12,12 +12,22 @@ app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Requested-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'POST, OPTIONS, PUT, DELETE');
-     res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Type', 'application/pdf');
+		res.setHeader("Content-Transfer-Encoding", "binary");
+		if(req.param('id')!==undefined){
+				let id=req.param('id').replace('&','');
+				let filename=id+'reporte.pdf';
+				res.setHeader('Content-Disposition', 'attachment; filename='+filename+'');
+		}
+
     next();
 });
 
-app.get('/',function(req,res){
-  res.status(200).send("SERVER API");
+app.get('/getFile/:id',function(req,res){
+	let id=req.param('id').replace('&','');
+	let finalpath=path+id+'reporte.pdf';
+	console.log(finalpath)
+  res.sendFile(finalpath);
 });
 app.use(api);
 app.listen(port);
