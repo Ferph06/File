89917dde40f730a8eaf003892812dbb58fe92342
unknown <fernando.perez@sferea.com>
2017-11-24 13:30:16 -0600

    creacion de otra ruta para un html con un style predeterminado desde antes de la peticion

--- Controllers/FileUpload.js
+++ Controllers/FileUpload.js
@@ -1,4 +1,5 @@
 'use strict'
+const Q=require('q');
 const htmltopdf=require('html-to-pdf')
 const fs = require('fs');
 const pathnormalize=('path');
@@ -11,12 +12,20 @@ const pdf = require('html-pdf');
 *@return res
 */
 const createHtml =(req,res) => {
+
+  let imgcc=".re-circle{width: 150px; height: 150px; box-sizing: border-box; float: left;padding-top: 5px; margin: 15px 30%;background: url(\"http://13.66.58.238:60617/Biossmann/resources/template/img/verificaciones/circle.png\") no-repeat;background-size: 100% 100%; text-align: center;} .re-left-div{width: 95%; min-height: 130px;box-sizing: border-box; float: right;margin: 0; padding: 0;}"
+
   let data=req.body;
   let pathn='reporte.pdf'
+
     if(data.html==undefined  || data.path==undefined || data.id==undefined || data.style==undefined) res.status(400).send("Hacen falta datos para procesar la peticion");
-      requestify.get(data.style).then((result) => {
-          let css=result.body;
-          let html="<html><head><meta charset=\"utf8\"><style>"+css+" </style></head><body>"+data.html+"</body></html>";
+      Q.all([requestify.get(data.style),
+          requestify.get('http://13.66.58.238:60617/Biossmann/javax.faces.resource/components.css.bsmn?ln=primefaces&v=6.0'),
+          requestify.get('http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css')
+      ]).then(datab => {
+          let cssR=datab[0].body+"\n"+datab[1].body+"\n"+datab[2].body;
+          let html="<html><head><meta charset=\"utf8\"> <style>"+cssR+" \n "+imgcc+"</style></head><body>"+data.html+"</body></html>";
+
           util.verifyDirectory(data.path).then(dir => {
               let finalname=(data.id.replace('&','').split('&').length>1)?data.id.replace('&','').split('&')[0]+"indicador":data.id.replace('&','');
               finalname=dir.path+finalname+pathn;
@@ -34,10 +43,34 @@ const createHtml =(req,res) => {
             }).catch(err =>{
             res.status(500).send("ERROR");
           });
-      }).catch((err) => {
-       res.status(500).send("ERROR");
-    });
+        }).catch( err => {
+          res.status(500).send("ERROR");
+      });
 }
+
+const  filePreHtml=(req,res) => {
+let data=req.body;
+let pathfinalname="";
+if(data.html==undefined || data.id==undefined || data.path==undefined) return res.status(400).send("Hacen falta datos en la peticion");
+util.verifyDirectory(data.path).then(result => {
+    if(result.path=='') return res.status(500).send("ERROR");
+      let finalname=data.id.replace('&','');
+      finalname=data.path+finalname+"ReporteEjecutivo.pdf";
+      pdf.create(data.html).toFile(finalname,(err,ok) => {
+          if (err) return res.status(500).send("ERROR");
+          let urlfile={
+            url:"http://13.66.58.238:5050/api/ReporteEjecutivo/"+data.id
+          }
+          res.status(200).send(urlfile);
+      });
+  }).catch(err => {
+    return res.status(500).send("ERROR");
+});
+
+}
+
+
 module.exports={
+  filePreHtml,
   createHtml
 }
--- routes.js
+++ routes.js
@@ -8,11 +8,20 @@ api.get('/',(req,res)=>{
 
   res.send("SERVER POWERED BY SFEREA...");
 
-})
+});
+
+
+api.get('/api/ReporteEjecutivo/:id', (req,res) => {
+    let id=req.params.id.replace('&','');
+    let filesend=path+id+"ReporteEjecutivo.pdf";
+    res.sendFile(filesend);
+});
+
+api.post('/api/FileHtmlPredefined',fileController.filePreHtml);
+
 api.get('/getFile/:id', (req,res) => {
 	let id=req.params.id.replace('&','');
   let finalname=(id.split('&').length==2)?id.split('&')[0]+"indicador":id.replace('&','');
-
   let filesend=path+finalname+'reporte.pdf';
     res.sendFile(filesend);
 });
